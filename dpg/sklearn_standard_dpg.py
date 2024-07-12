f308674a9b3521b87f65cd510611ec3031d9565e
Leonardo Arrighi <s232661@ds.units.it>
2024-07-12 16:37:04 +0200

    standard_sklearn fixed

--- dpg/sklearn_standard_dpg.py
+++ dpg/sklearn_standard_dpg.py
@@ -10,11 +10,18 @@ from sklearn.datasets import (
 )
 
 from .core import digraph_to_nx, get_dpg, get_dpg_node_metrics, get_dpg_metrics
-from .visualizer import plot_dpg#, plot_custom_map#, plot_communities_map
-
-
+from .visualizer import plot_dpg
 
 def select_dataset(name):
+    """
+    Selects a standard sklearn dataset based on the provided name.
+
+    Args:
+    name: The name of the dataset to load.
+
+    Returns:
+    The selected dataset.
+    """
     datasets = {
         "iris": load_iris(),
         "diabetes": load_diabetes(),
@@ -25,39 +32,37 @@ def select_dataset(name):
 
     return datasets.get(name.lower(), None)
 
-# def select_custom_dataset(path, target_column):
-#     df = pd.read_csv(path, sep=',')
-#     target = df[target_column]
-#     df.drop(columns=[target_column], inplace=True)
-#     df_cat = df.select_dtypes('object')
-#     df_categorical = df_cat.columns
-#     preprocessor = ColumnTransformer(
-#         transformers=[('onehot', OneHotEncoder(), df_categorical)],
-#         remainder='passthrough')
-#     data = preprocessor.fit_transform(df)
-#     features = preprocessor.get_feature_names_out()
 
-#     return data, features, target
+def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, file_name=None, plot=False, save_plot_dir="examples/", attribute=None, communities=False, class_flag=True):
+    """
+    Trains a Random Forest classifier on a selected dataset, evaluates its performance, and optionally plots the DPG.
 
-def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, plot=False, file_name=None):
+    Args:
+    datasets: The name of the dataset to use.
+    n_learners: The number of trees in the Random Forest.
+    perc_var: Threshold value indicating the desire to retain only those paths that occur with a frequency exceeding a specified proportion across the trees.
+    decimal_threshold: Decimal precision of each feature. 
+    file_name: The name of the file to save the evaluation results. If None, prints the results to the console.
+    plot: Boolean indicating whether to plot the DPG. Default is False.
+    save_plot_dir: Directory to save the plot image. Default is "examples/".
+    attribute: A specific node attribute to visualize. Default is None.
+    communities: Boolean indicating whether to visualize communities. Default is False.
+    class_flag: Boolean indicating whether to highlight class nodes. Default is True.
+
+    Returns:
+    df: A pandas DataFrame containing node metrics.
+    df_dpg: A pandas DataFrame containing DPG metrics.
+    """
     
     # Load dataset
     dt = select_dataset(datasets)
-
-    # # Load custom dataset
-    # data, features, target = select_custom_dataset(datasets, target_column = 'HeartDisease')
     
-    # Split dataset
+    # Split dataset into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(
         dt.data, dt.target, test_size=0.3, random_state=42
     )
-
-    # # Split custom dataset
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     data, target, test_size=0.3, random_state=42
-    # )
     
-    # RF
+    # Train Random Forest classifier
     rf_classifier = RandomForestClassifier(n_estimators=n_learners, random_state=42)
     rf_classifier.fit(X_train, y_train)
     y_pred = rf_classifier.predict(X_test)
@@ -67,16 +72,15 @@ def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, plot=Fa
     confusion = confusion_matrix(y_test, y_pred)
     classification_rep = classification_report(y_test, y_pred)
 
-    # Print the results
+    # Print or save the evaluation results
     if file_name is not None:
-        f = open(file_name, "w")
-        f.write(f'Accuracy: {accuracy:.2f}\n')
-        f.write('\nConfusion Matrix:\n')
-        for i in confusion:
-            f.write(f'{str(i)}\n')
-        f.write('\nClassification Report:')
-        f.write(classification_rep)
-        f.close()
+        with open(file_name, "w") as f:
+            f.write(f'Accuracy: {accuracy:.2f}\n')
+            f.write('\nConfusion Matrix:\n')
+            for i in confusion:
+                f.write(f'{str(i)}\n')
+            f.write('\nClassification Report:')
+            f.write(classification_rep)
     else:
         print(f'Accuracy: {accuracy:.2f}')
         print('Confusion Matrix:')
@@ -84,21 +88,21 @@ def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, plot=Fa
         print('Classification Report:')
         print(classification_rep)
 
-    # dpg Extraction
+    # Extract DPG
     dot = get_dpg(X_train, dt.feature_names, rf_classifier, perc_var, decimal_threshold)
     
-    # # dpg Extraction custom dataset
-    # dot = get_dpg(X_train, features, rf_classifier, perc_var, decimal_threshold)
-    
+    # Convert Graphviz Digraph to NetworkX DiGraph
     dpg_model, nodes_list = digraph_to_nx(dot)
 
     if len(nodes_list) < 2:
         print("Warning: Less than two nodes resulted.")
         return
     
+    # Get metrics from the DPG
     df_dpg = get_dpg_metrics(dpg_model, nodes_list)
     df = get_dpg_node_metrics(dpg_model, nodes_list)
     
+    # Plot the DPG if requested
     if plot:
         plot_name = (
             datasets
@@ -115,9 +119,10 @@ def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, plot=Fa
             dot,
             df,
             df_dpg,
-            attribute='Betweness centrality',
-            # communities=True,
-            class_flag=True
+            save_dir=save_plot_dir,
+            attribute=attribute,
+            communities=communities,
+            class_flag=class_flag
         )
     
     return df, df_dpg
