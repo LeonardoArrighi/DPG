69550c7300a69a7c00a6859182a6baab3fb3d4e0
Leonardo Arrighi <s232661@ds.units.it>
2024-11-14 11:19:28 +0100

    Add the possibility to applicate different sklearn classifiers

--- dpg/sklearn_standard_dpg.py
+++ dpg/sklearn_standard_dpg.py
@@ -1,5 +1,5 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, GradientBoostingClassifier, BaggingClassifier, ExtraTreesClassifier, AdaBoostClassifier, AdaBoostRegressor
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, f1_score, mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import (
     load_iris,
@@ -8,6 +8,7 @@ from sklearn.datasets import (
     load_breast_cancer,
     load_diabetes,
 )
+from sklearn.base import is_classifier, is_regressor
 
 from .core import digraph_to_nx, get_dpg, get_dpg_node_metrics, get_dpg_metrics
 from .visualizer import plot_dpg
@@ -33,7 +34,7 @@ def select_dataset(name):
     return datasets.get(name.lower(), None)
 
 
-def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, file_name=None, plot=False, save_plot_dir="examples/", attribute=None, communities=False, class_flag=False):
+def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, model_name='RandomForestClassifier', file_name=None, plot=False, save_plot_dir="examples/", attribute=None, communities=False, class_flag=False):
     """
     Trains a Random Forest classifier on a selected dataset, evaluates its performance, and optionally plots the DPG.
 
@@ -42,6 +43,7 @@ def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, file_na
     n_learners: The number of trees in the Random Forest.
     perc_var: Threshold value indicating the desire to retain only those paths that occur with a frequency exceeding a specified proportion across the trees.
     decimal_threshold: Decimal precision of each feature. 
+    model_name: The name of the model chosen. Default is RandomForestClassifier.
     file_name: The name of the file to save the evaluation results. If None, prints the results to the console.
     plot: Boolean indicating whether to plot the DPG. Default is False.
     save_plot_dir: Directory to save the plot image. Default is "examples/".
@@ -62,34 +64,67 @@ def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, file_na
         dt.data, dt.target, test_size=0.3, random_state=42
     )
     
-    # Train Random Forest classifier
-    rf_classifier = RandomForestClassifier(n_estimators=n_learners, random_state=42)
-    rf_classifier.fit(X_train, y_train)
-    y_pred = rf_classifier.predict(X_test)
+    # Train model
+    if model_name == 'RandomForestClassifier':
+        model = RandomForestClassifier(n_estimators=n_learners, random_state=42)
+    elif model_name == 'ExtraTreesClassifier':
+        ExtraTreesClassifier(n_estimators=n_learners, random_state=42)
+    elif model_name == 'AdaBoostClassifier':
+        model = AdaBoostClassifier(n_estimators=n_learners, random_state=42)
+    elif model_name == 'BaggingClassifier':
+        BaggingClassifier(n_estimators=n_learners, random_state=42)
+    else:
+        raise Exception("The selected model is not currently available.")
+    
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     
     # Evaluate the model
     accuracy = accuracy_score(y_test, y_pred)
     confusion = confusion_matrix(y_test, y_pred)
     classification_rep = classification_report(y_test, y_pred)
 
-    # Print or save the evaluation results
-    if file_name is not None:
-        with open(file_name, "w") as f:
-            f.write(f'Accuracy: {accuracy:.2f}\n')
-            f.write('\nConfusion Matrix:\n')
-            for i in confusion:
-                f.write(f'{str(i)}\n')
-            f.write('\nClassification Report:')
-            f.write(classification_rep)
-    else:
-        print(f'Accuracy: {accuracy:.2f}')
-        print('Confusion Matrix:')
-        print(confusion)
-        print('Classification Report:')
-        print(classification_rep)
+    if is_classifier(model):
+        # Evaluate the model
+        accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred, average='weighted')
+        confusion = confusion_matrix(y_test, y_pred)
+        classification_rep = classification_report(y_test, y_pred)
+
+        # Print or save the evaluation results
+        if file_name is not None:
+            with open(file_name, "w") as f:
+                f.write(f'Statistics for the model: {model_name}\n\n')
+                f.write(f'Accuracy: {accuracy:.2f}\n')
+                f.write(f'F1 Score: {f1:.2f}\n')
+                f.write('\nConfusion Matrix:\n')
+                for i in confusion:
+                    f.write(f'{str(i)}\n')
+                f.write('\nClassification Report:')
+                f.write(classification_rep)
+        else:
+            print(f'Accuracy: {accuracy:.2f}')
+            print('Confusion Matrix:')
+            print(confusion)
+            print('Classification Report:')
+            print(classification_rep)
+
+    elif is_regressor(model):
+        # Evaluate the model
+        mse = mean_squared_error(y_test, y_pred)
+
+        # Print or save the evaluation results
+        if file_name is not None:
+            with open(file_name, "w") as f:
+                f.write(f'Statistics for the model: {model_name}\n\n')
+                f.write(f'Mean Squared Error: {mse:.2f}')
+        else:
+            print(f"Mean Squared Error: {mse:.2f}")
+
+
 
     # Extract DPG
-    dot = get_dpg(X_train, dt.feature_names, rf_classifier, perc_var, decimal_threshold)
+    dot = get_dpg(X_train, dt.feature_names, model, perc_var, decimal_threshold)
     
     # Convert Graphviz Digraph to NetworkX DiGraph
     dpg_model, nodes_list = digraph_to_nx(dot)
@@ -106,6 +141,8 @@ def test_base_sklearn(datasets, n_learners, perc_var, decimal_threshold, file_na
     if plot:
         plot_name = (
             datasets
+            + "_"
+            + model_name
             + "_bl"
             + str(n_learners)
             + "_perc"
