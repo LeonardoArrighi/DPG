a2436de5839286ebe73483ba7dc67669109cc9b2
sbarbonjr <sbarbonjr@gmail.com>
2025-04-22 18:46:49 +0200

    Implement parallel processing in tracing_ensemble for improved performance

--- dpg/core.py
+++ dpg/core.py
@@ -30,9 +30,15 @@ class DecisionPredicateGraph:
         print("Model Params: ", self.model.get_params())
         print("*****************************************************************")
 
-        log = Parallel(n_jobs=self.n_jobs)(
-            delayed(self.tracing_ensemble)(i, sample) for i, sample in tqdm(list(enumerate(X_train)), total=len(X_train))
-        )
+
+        if self.n_jobs == 1:
+            log = Parallel(n_jobs=self.n_jobs)(
+                delayed(self.tracing_ensemble)(i, sample) for i, sample in tqdm(list(enumerate(X_train)), total=len(X_train))
+            )
+        else:
+            log = Parallel(n_jobs=self.n_jobs)(
+                delayed(self.tracing_ensemble_parallel)(i, sample) for i, sample in tqdm(list(enumerate(X_train)), total=len(X_train))
+            )
 
         log = [item for sublist in log for item in sublist]
         log_df = pd.DataFrame(log, columns=["case:concept:name", "concept:name"])
@@ -80,6 +86,42 @@ class DecisionPredicateGraph:
                     node_index = right
                 yield [prefix, condition]
 
+    #for parallel processing, when using n_jobs>1
+    def tracing_ensemble_parallel(self, case_id, sample):
+        is_regressor = isinstance(self.model, (RandomForestRegressor, ExtraTreesRegressor, AdaBoostRegressor))
+        sample = sample.reshape(-1)
+        result = []
+        for i, tree in enumerate(self.model.estimators_):
+            tree_ = tree.tree_
+            node_index = 0
+            prefix = f"sample{case_id}_dt{i}"
+            while True:
+                left = tree_.children_left[node_index]
+                right = tree_.children_right[node_index]
+                if left == right:
+                    if is_regressor:
+                        pred = round(tree_.value[node_index][0][0], 2)
+                        result.append([prefix, f"Pred {pred}"])
+                    else:
+                        pred_class = tree_.value[node_index].argmax()
+                        if self.target_names is not None:
+                            pred_class = self.target_names[pred_class]
+                        result.append([prefix, f"Class {pred_class}"])
+                    break
+                feature_index = tree_.feature[node_index]
+                threshold = round(tree_.threshold[node_index], self.decimal_threshold)
+                feature_name = self.feature_names[feature_index]
+                sample_val = sample[feature_index]
+                if sample_val <= threshold:
+                    condition = f"{feature_name} <= {threshold}"
+                    node_index = left
+                else:
+                    condition = f"{feature_name} > {threshold}"
+                    node_index = right
+                result.append([prefix, condition])
+        return result
+            
+
     def filter_log(self, log):
         from collections import defaultdict
         variant_map = defaultdict(list)
@@ -95,9 +137,10 @@ class DecisionPredicateGraph:
         return log[log["case:concept:name"].isin(case_ids_to_keep)].copy()
 
     def discover_dfg(self, log):
+
         def process_chunk(chunk):
             chunk_dfg = {}
-            for case in chunk:
+            for case in tqdm(chunk, desc="Processing cases", leave=False):
                 trace_df = log[log["case:concept:name"] == case].copy()
                 trace_df.sort_values(by="case:concept:name", inplace=True)
                 for i in range(len(trace_df) - 1):
